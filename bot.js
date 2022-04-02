cdbc31101a97776d11565cc5954913ffec535c48
yosogo <80201392+yosogo@users.noreply.github.com>
2022-04-02 15:50:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 var TelegramBot = require('node-telegram-bot-api');
-var tkn = '1745323024:AAF8NJQfOlMOPL02YHbvXALL5BozJK6WOMw';
-var btn = new TelegramBot(tkn, {polling: true});
+var tokn = '1745323024:AAF8NJQfOlMOPL02YHbvXALL5BozJK6WOMw';
+var bot = new TelegramBot(tokn, {polling: true});
 
-btn.on("polling_error", (err) => console.log(err));
-btn.on("message",function(msg){
-btn.sendMessage(msg.chat.id,rep, {msg.text});
+bot.on("polling_error", (err) => console.log(err));
+bot.on("message",function(msg){
+bot.sendMessage(msg.chat.id,rep, {msg.text});
 });
