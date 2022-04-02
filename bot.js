fe3e58dbac572b93dc1a1de3b0b0696cb047e198
yosogo <80201392+yosogo@users.noreply.github.com>
2022-04-02 16:12:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 var TelegramBot = require('node-telegram-bot-api');
-var tokn = '1745323024:AAF8NJQfOlMOPL02YHbvXALL5BozJK6WOMw';
-var bot = new TelegramBot(tokn, {polling: true});
+var token = '1745323024:AAF8NJQfOlMOPL02YHbvXALL5BozJK6WOMw';
+var bot = new TelegramBot(token, {polling: true});
 
 bot.on("polling_error", (err) => console.log(err));
 bot.on("message",function(msg){
